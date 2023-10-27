3dafdbe0aa1248742e4384bd3d88ad3dba2de968
Aarav Arya <aarav.arya2307@gmail.com>
2023-10-27 17:28:26 +0530

    add showAllTodos function

--- main/index.js
+++ main/index.js
@@ -51,3 +51,36 @@ add_btn.addEventListener("click", () => {
 });
 
 delete_all_btn.addEventListener("click", clearAllTodos);
+
+function showAllTodos(){
+    todos_list_body.innerHTML = "";
+    if (todos.length === 0){
+      todos_list_body.innerHTML = 
+      `<tr>
+        <td colspan="5" class="text-center">No task found</td>
+      </tr>`;
+    }
+    else{
+        todos.forEach((todo) => {
+            todos_list_body.innerHTML += `
+                    <tr class="todo-item" data-id="${todo.id}">
+                        <td class="divScroll-1">${todo.task}</td>
+                        <td>${todo.dueDate || "No due date"}</td>
+                        <td>${todo.status}</td>
+                        <td>
+                            <button class="btn btn-warning btn-sm" onclick="editTodo('${todo.id}')">
+                                <i class="bx bx-edit-alt bx-bx-xs"></i>    
+                            </button>
+                            <button class="btn btn-success btn-sm" onclick="toggleStatus('${todo.id}')">
+                                <i class="bx bx-check bx-xs"></i>
+                            </button>
+                            <button class="btn btn-error btn-sm" onclick="deleteTodo('${todo.id}')">
+                                <i class="bx bx-trash bx-xs"></i>
+                            </button>
+                        </td>
+                    </tr>
+                `;
+          });
+    }  
+  }
+
